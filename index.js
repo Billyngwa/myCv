66cf185d425ba4748a1477f67e6a902b3d042453
Billyngwa <billytatangwa5@gmail.com>
2022-12-06 00:21:59 -0800

    just pushed

---
+++ index.js
@@ -0,0 +1,16 @@
+function questionThree(){
+    let myArray = [2,7,3,6,4];
+    let arrayIndex = 1;
+    
+    let starter = myArray[0];
+    
+    for(arrayIndex; arrayIndex <= myArray.length; arrayIndex++){
+        
+ 
+        sum = starter + myArray[arrayIndex];
+    }
+
+    console.log(sum);
+}
+
+console.log(questionThree());
